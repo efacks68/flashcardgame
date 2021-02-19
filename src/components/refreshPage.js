99269b8b7966a27a9516a065961e6ea2b8f21c75
efacks68 <fackelman.eric@gmail.com>
2021-02-19 14:20:45 +0100

    Made buttons display translation

---
+++ src/components/refreshPage.js
@@ -0,0 +1,8 @@
+import React from "react"
+//from https://upmostly.com/tutorials/how-to-refresh-a-page-or-component-in-react
+
+function refreshPage() {
+    window.location.reload(false);
+}
+
+export default refreshPage
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import styled from 'styled-components'; //for editing button styling
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import styled from "styled-components"; //for editing button styling
 import RandInt from "./components/RandInt"
+import refreshPage from "./components/refreshPage"
 
 const Button = styled.button`font-size:20px;` //to make the button font larger, default was too small
 
 //Make arrays of words for proving basic functionality
-const english=['man','woman','boy','girl'];
-const espanol=['hombre','mujer','niño','niña'];
-const norsk=['mann','kvinne','gutt','jenta'];
+const english=["man","woman","boy","girl"];
+const espanol=["hombre","mujer","niño","niña"];
+const norsk=["mann","kvinne","gutt","jenta"];
 
-const num=RandInt(0,3); //determines the 'random' index value. Fixed bounds due to small test arrays
+//determines the "random" index value. 
+var num=RandInt(0,3);
 
 //Foundation for functionality, returns word to translate
 class Word extends React.Component {
@@ -22,31 +24,59 @@ class Word extends React.Component {
   }
 }
 
-//simple function to trigger alert when button is clicked
-function palabra() {
-  alert(espanol[num]); 
+//class to change text that is displayed in button when button is clicked
+class Palabra extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: "Español",};
+  }
+  render() {
+    return (
+      <Button 
+        className="idioma" 
+        onClick={() => this.setState({value:espanol[num]})}>
+        {this.state.value}
+      </Button>
+    );
+  }
 }
 
-//simple function to trigger alert when button is clicked
-function ord() {
-  alert(norsk[num]); 
+//class to change the text that is displayed in button when button is clicked
+class Ord extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: "Norsk"};
+  }
+  render() {
+    return(
+      <Button
+        className="språk"
+        onClick={() => this.setState({value:norsk[num]})}>
+        {this.state.value}
+      </Button>
+    );
+  }
 }
 
 //Component to render
+//added Next Button to refresh the page and get a new word
+//next steps include getting game to cycle through the remaining words, not showing previously displayed ones, but still thinking about how to do that...
 class FlashCards extends React.Component {
   render() {
     return (
       <div className="flash-cards">
         <div className="to-translate">
           <h1>Translate:</h1>
-          <h2>&nbsp;<Word /></h2>
-          <Button 
-            onClick={palabra}
-            >Español</Button> 
-            &nbsp;&nbsp; 
+          <h2><Word /></h2>
+          <div>
+            <Palabra />
+              &nbsp;&nbsp;
+            <Ord />
+            <br/><br/><br/>
+          </div>
           <Button
-            onClick={ord}
-            >Norsk</Button>
+            onClick={refreshPage}
+          >Next</Button>
         </div>
       </div>
     );
@@ -56,5 +86,5 @@ class FlashCards extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <FlashCards />,document.getElementById('root')
+  <FlashCards />,document.getElementById("root")
 );     
